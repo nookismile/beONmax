500ff208491b1c75dcbb6bc10b7335b2f30be618
nookismile <stina.novickova@yandex.ru>
2020-12-15 13:18:48 +0300

    add cycles

--- script.js
+++ script.js
@@ -17,8 +17,49 @@ for (let i = 0; i < 2; i++) {
     if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
         console.log('done');
         appData.expenses[a] = b;
-    } else {}
+    } else {
+      console.log('fail');
+      i--;
+    }
 };
+
+
+// цикл DO...WHILE
+//
+//let i = 0;
+//
+//do {
+//   let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//        b = prompt('Во сколько обойдется?', '');
+//
+//    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//        console.log('done');
+//        appData.expenses[a] = b;
+//    } else {
+//      console.log('fail');
+//      alert("Неверно введены данные");
+//    }
+//  i++;
+//} while (i < 2);
+//
+//
+//цикл WHILE
+//
+//while(i < 2) {
+//  let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//        b = prompt('Во сколько обойдется?', '');
+//
+//    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//        console.log('done');
+//        appData.expenses[a] = b;
+//    } else {
+//      console.log('fail');
+//      alert("Неверно введены данные");
+//    }
+//
+//  i++;
+//}
+
 
 appData.moneyPerDay = appData.budget / 30;
 
